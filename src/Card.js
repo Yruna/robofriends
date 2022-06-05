c6a97e55019dfefa083fe102bbd540782cc53d49
Yruna <kayhantunaikin@gmail.com>
2022-06-05 23:09:37 +0300

    SearchBox eklendi.

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = ({ name, email, id }) => {
     return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src={`https://robohash.org/${id}?200x200`} alt="robots"/>
+        <div className="bg-light-green dib br3 pa3 ma2 tc grow shadow-5">
+            <img src={`https://robohash.org/${id}?size=200x200`} alt="robots"/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>

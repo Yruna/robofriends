01ce35af624cdea532ddee3abe95d7d9e230be81
Yruna <kayhantunaikin@gmail.com>
2022-08-16 15:51:01 +0300

    Added font and background.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
 import { robots } from './robots';
+import './App.css';
 
 class App extends Component {
     constructor() {
@@ -11,15 +12,22 @@ class App extends Component {
             searchfield: ''
         }
     }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
     render() {
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
         return (
             <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={this.state.robots}/>
+            <h1 className='f2'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
             </div>
         );
     }
 }
-
 export default App;
